4b5e1aa4ec120a0970807d13882c6d17f473e5d8
Mary Chill <chilikina.mari@inbox.ru>
2023-10-13 12:51:35 +0300

    errors-img

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { ReactComponent as  Line} from '../img/line.svg';
-import { ReactComponent as  Block} from '../img/block.svg';
+//import { ReactComponent as  Line} from '../img/line.svg';
+//import { ReactComponent as  Block} from '../img/block.svg';
 
-export const Icon = (name) => {
-    if (name === 'line') return <Line />;
-    if (name === 'block') return <Block />;
+export const Icon = (name, sw) => {
+    sw();
+    if (name === 'line') return (
+        <svg xmlns = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+            <path d=''></path>
+        </svg>
+        )
+    if (name === 'block') return (
+        <svg xmlns = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+            <path d=''></path>
+        </svg>
+        )
 }
