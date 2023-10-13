05f9bd72a75ab4b672d942ec8216db5227d4d119
Mary Chill <chilikina.mari@inbox.ru>
2023-10-13 15:54:25 +0300

    errors-toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,21 @@ import { useState } from 'react';
 import './App.css';
 import React from 'react';
 import { Icon } from './Icon';
+import { products } from './Products';
 
 const Store = () => {
- const [position, setPosition] = useState({position:true});
+
+ const [position, setPosition] = useState(false);
 
   const toggle = () => {
-    console.log(Object.values(position)[0]);
-    setPosition(({position}) => ({position : !position}));
+    setPosition(position => !position);
   }
 
-  const products = [{
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "red",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "green",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "blue",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "black",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
-  }, {
-    name: "Nike free run",
-    price: "170",
-    color: "black",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg"
-  }, {
-    name: "Nike Metcon 3",
-    price: "150",
-    color: "green",
-    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/5.jpg"
-  }];
+  
 
   return (
     <>
-      <IconSwitch icon={Object.values(position)[0] ? 'line' : 'block'} onSwitch = {toggle} />
+      <IconSwitch icon={position ? 'line' : 'block'} onSwitch = {toggle} />
       <CardsView cards={products} />
       <ListView items={products} />
     </>
